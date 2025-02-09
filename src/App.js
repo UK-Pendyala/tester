dc5720399dfe3d66c56ed001e4a4e79002cc4629
Umakanth Pendyala <pendyalu@oregonstate.edu>
2025-02-09 02:27:40 -0800

    inital commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className = "header">
+      Hello my name is umakanth, this page is under construction
+    </div>
+  );
+}
+
+export default App;
